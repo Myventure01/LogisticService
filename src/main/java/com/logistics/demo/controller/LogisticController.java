bfcf1fe8256fa714b8016d681c4dd05f88e51d21
Myventure01 <101160276+Myventure01@users.noreply.github.com>
2022-10-02 17:32:10 +0530

    Update LogisticController.java

--- src/main/java/com/logistics/demo/controller/LogisticController.java
+++ src/main/java/com/logistics/demo/controller/LogisticController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import com.logistics.demo.model.UpdateUserLocModel;
 import com.logistics.demo.model.UserLatestLocationModel;
 import com.logistics.demo.model.UserLocationModel;
